b48f77f512f7f40882c7b22a12f8a5d24df114ed
victor montebello <montebellovictor@gmail.com>
2024-02-29 18:59:51 -0300

    another version of the same problem

--- Amazon/AddTwoNumbers/AddTwoNumbers.go
+++ Amazon/AddTwoNumbers/AddTwoNumbers.go
@@ -24,3 +24,27 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
     return result.Next
 }
+
+//Another version
+/*func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    result := &ListNode{}
+    tmp := result
+    for l1 != nil || l2 != nil {
+        if l1 != nil {
+            tmp.Val += l1.Val
+            l1 = l1.Next
+        }
+        if l2 != nil {
+            tmp.Val += l2.Val
+            l2 = l2.Next
+        }
+        if tmp.Val > 9 {
+            tmp.Val -= 10
+            tmp.Next = &ListNode{Val: 1}
+        } else if l1 != nil || l2 != nil {
+            tmp.Next = &ListNode{}
+        }
+        tmp = tmp.Next
+    }
+    return result
+}*/
